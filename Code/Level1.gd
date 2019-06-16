7a495d0544b9080e7b1e15ed2c03c340f452afd4
arlr <arlrcode@gmail.com>
2019-06-16 23:11:14 +0200

    Prise en compte de plusieurs pills pour le HUD et recréationdes lvl 1 et 2

--- Code/Level1.gd
+++ Code/Level1.gd
@@ -7,7 +7,7 @@ var counterPill = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
+	
 func _input(event):
 	
 	if Input.is_action_just_pressed("ui_cancel"):
--- Code/Level2.gd
+++ Code/Level2.gd
@@ -5,11 +5,10 @@ extends Spatial
 # var b = "text"
 
 
-var counterPill
+var counterPill =0
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	counterPill = 12
-
+	
 func _input(event):
 	
 	if Input.is_action_just_pressed("ui_cancel"):
