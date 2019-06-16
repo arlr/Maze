1e835cff831ce8adef296c0ca3105cc8d7b15101
arlr <arlrcode@gmail.com>
2019-06-16 17:43:20 +0200

    rEFONTE DU LEVEL 1 AVEC GRIDmAP

--- Levels/Level1.gd
+++ Levels/Level1.gd
@@ -6,8 +6,9 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input(event):
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
