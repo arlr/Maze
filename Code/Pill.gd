b1297521b8f0155a87283aa49f6c6bee6e7bf322
arlr <arlrcode@gmail.com>
2019-06-16 21:20:43 +0200

    Debut creation HUD

--- Code/Pill.gd
+++ Code/Pill.gd
@@ -3,10 +3,12 @@ extends Area
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var counter = 0
+onready var LabelCounter = get_node("Player/HUD/NombrePill")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	counter = 0
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,5 +19,7 @@ func _process(delta):
 
 func _on_Area_body_entered(body):
 	if body is KinematicBody:
+		counter += 1
 		print("Collect 1 Pill")
+		LabelCounter.set_text("Nombre de Pill : "+str(counter))
 		queue_free()
