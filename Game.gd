926cedeaa6d4a4431de3d6f7b2e7eb9470cf6a0d
arlr <arlrcode@gmail.com>
2019-06-16 16:39:06 +0200

    Ajout Souris

---
+++ Game.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
