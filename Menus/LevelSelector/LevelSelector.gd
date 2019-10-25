07738931326f28420cf7277b3674092709c5e709
Arthur <arlrcode@gmail.com>
2019-10-25 20:11:55 +0200

    Les niveux dans LevelSelecteur sont maintenant gérer automatiquement en fonction des dossiers dans *Levels* les boutons sont automatquement linké vers les scenes des niveux

--- Menus/LevelSelector/LevelSelector.gd
+++ Menus/LevelSelector/LevelSelector.gd
@@ -10,13 +10,21 @@ func _ready():
 	var node = get_node("VBoxContainer/GridContainer")
 	print_debug(get_node("VBoxContainer/GridContainer").get_children())
 	for nb in range(VariableGlobal.Nb_Level):
-		var bp_name = "LV "+str(nb+1)+" Perso"
-		print("LV "+str(nb+1)+"Perso")
+		var bp_name = "LV "+str(nb+1)
+		print("LV "+str(nb+1))
+		# Setup the button
 		var bp = Button.new()
 		bp.name = bp_name
 		bp.text = bp_name
 		bp.flat = true
+		bp.rect_min_size = Vector2(130, 130)
 		bp.ACTION_MODE_BUTTON_PRESS
+		bp.add_font_override("font", load("res://Fonts/Menu_FontDynamic.tres"))
+		bp.connect("pressed", self , "_on_autoBp_pressed", [bp])
+		#bg.("pressed",self, "_on_autoBp_pressed")
+		print_debug(bp.name)
+		
+	
 		
 		node.add_child(bp)
 
@@ -27,14 +35,15 @@ func _ready():
 func _on_Retour_pressed():
 	VariableGlobal._deferred_goto_scene(VariableGlobal.old_scene)
 
-func _on_Lv1_pressed():
-	VariableGlobal._deferred_goto_scene("res://Levels/Level1/Level1.tscn")
-	VariableGlobal.actual_lvl = 1
-
-func _on_Lv2_pressed():
-	VariableGlobal._deferred_goto_scene("res://Levels/Level2/Level2.tscn")
-	VariableGlobal.actual_lvl = 2
-
-func _on_Lv3_pressed():
-	VariableGlobal._deferred_goto_scene("res://Levels/Level3/Level3.tscn")
-	VariableGlobal.actual_lvl = 3
+func _on_autoBp_pressed(target):
+	#Basic function for every automaticly created button 
+	print_debug("In pressed func")
+	var name_target = target.get_name()
+	print_debug(name_target.split(' ')[1].replace(' ', ''))
+	
+	var scene_target = "Level" + name_target.split(' ')[1].replace(' ', '') 
+	print_debug("scene target = " + scene_target)
+	
+	var path_scene_target = "res://Levels/" + scene_target + "/" + scene_target +".tscn"
+	VariableGlobal._deferred_goto_scene(path_scene_target)
+	VariableGlobal.actual_lvl = int(name_target.split(' ')[1].replace(' ', ''))
